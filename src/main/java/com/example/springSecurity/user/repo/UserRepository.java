d628345b27b4e711ec75ea5335ce29f3df3c8881
MeangSung <aoddlsgh98@gmail.com>
2025-02-05 00:01:57 +0900

    UserRepository 생성

--- src/main/java/com/example/springSecurity/user/repo/UserRepository.java
+++ src/main/java/com/example/springSecurity/user/repo/UserRepository.java
@@ -3,12 +3,11 @@ package com.example.springSecurity.user.repo;
 import com.example.springSecurity.user.repo.entity.User;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    @Query("select u from User u "
-            + "where u.nickname = :username")
-    Optional<User> findByUsername(@Param("username")String username);
+    Optional<User> findByEmail(String username);
+
+    Optional<User> findByNickname(String nickname);
+
 }
