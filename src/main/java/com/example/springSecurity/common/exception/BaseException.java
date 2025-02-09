489f50ce675d3e3746da4f143f0c47476113804d
MeangSung <aoddlsgh98@gmail.com>
2025-02-10 00:24:14 +0900

    불필요한 클래스 제거

--- src/main/java/com/example/springSecurity/common/exception/BaseException.java
+++
@@ -1,9 +0,0 @@
-package com.example.springSecurity.common.exception;
-
-import lombok.Getter;
-
-@Getter
-public class BaseException extends Exception {
-
-  private BaseResponseStatus baseResponseStatus;
-}
